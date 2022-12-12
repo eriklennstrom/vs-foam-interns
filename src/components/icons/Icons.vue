1c0bab7d4c840dca298623be368f36edd87742da
timand1 <timandersson22@live.se>
2022-12-12 14:58:47 +0100

    Icons component /w storybook

---
+++ src/components/icons/Icons.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">import { ref, type Ref } from 'vue';
+
+    const props = defineProps(['iconType', 'color'])
+    const iconType : Ref = ref<string>(props.iconType)
+    const iconColor : Ref = ref<string>(props.color)
+    
+</script>
+
+<template>
+    <i class="bi" :class="{[iconType]: true}" style="font-size: 2rem;" :style="{ color: props.color}"></i>
+</template>
+
+<style>
+.icon {
+    font-size: 2em;
+    width: 5em;
+    height: 5em;
+
+}
+</style>
