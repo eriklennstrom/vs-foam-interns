66e41cdf9e52dce0623048e573faef3cbca0147c
timand1 <timandersson22@live.se>
2022-12-16 15:39:09 +0100

    Added test for icons

--- src/components/icons/icons.spec.ts
+++ src/components/icons/icons.spec.ts
@@ -3,33 +3,35 @@ import { mount } from '@vue/test-utils'
 import Icons from './icons.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret, faWarning, faArrowDown, faArrowAltCircleDown} from  '@fortawesome/free-solid-svg-icons';
+import exp from 'constants';
 library.add( faUserSecret, faWarning, faArrowDown, faArrowAltCircleDown)
 
 describe('Icons', () => {
 
   it('renders properly', () => {
     const wrapper = mount(Icons, { propsData: { variant:'warning' } })
-    console.log(wrapper.classes())
     expect(wrapper.classes()).toContain('fa-warning')
   })  
 
+  it('renders correct variant prop', () => {
+    const wrapper = mount(Icons, { propsData: { variant:'warning' } })
+    expect(wrapper.vm.variant).toBe('warning');
+  })
+
   it('renders color', () => {
     const wrapper = mount(Icons, { propsData: { variant:'warning', color:'success' } })
-    expect(getComputedStyle(wrapper.element).color == '#55c284')
+    expect(wrapper.vm.color).toBe('success');
+    expect(wrapper.classes()).toContain('success')
   })    
+ 
+  it('renders correct color prop and class with no input', () => {
+    const wrapper = mount(Icons)
+    expect(wrapper.vm.color).toBe('primary');
+    expect(wrapper.classes()).toContain('primary')
+  })   
 
-  it('renders default color with no input', () => {
-    const wrapper = mount(Icons, { propsData: { variant:'warning' } })
-    expect(getComputedStyle(wrapper.element).color == 'rgb(0, , 0)')
-  })     
-
-  it('renders default color with wrong input', () => {
-    const wrapper = mount(Icons, { propsData: { variant:'warning', color:'yelolw' } })
-    expect(getComputedStyle(wrapper.element).color == 'rgb(0, 0, 0)')
-  })  
   it('render correct size', () => {
     const wrapper = mount(Icons, { propsData: { variant:'warning', size: 18} })
-    console.log(getComputedStyle(wrapper.element).height)
     expect(getComputedStyle(wrapper.element).fontSize).toBe('18px')
   })      
 
