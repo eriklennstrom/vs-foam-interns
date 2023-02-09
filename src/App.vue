8a46f3b8b88d9a17bcdc6ab2cafaec6e073ad14c
altffyra <chjohnsson@hotmail.com>
2023-02-09 13:18:15 +0100

    size changes

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ const test= ref('')
   <h1>{{ test }}</h1>
   
   <Inputvue
-    size="S"
+    size="sm"
     v-model="test"
     validation-text="Validation-text"
     variant="number"
@@ -46,7 +46,7 @@ const test= ref('')
   />
   <Inputvue
     v-model="test"
-    size="S"
+    size="sm"
     :is-valid="false"
     validation-text="Validation-text"
     variant="text"
@@ -58,7 +58,7 @@ const test= ref('')
 
   <Inputvue
     v-model="test"
-    size="M"
+    size="lg"
     variant="text"
     text="Neutral"
     placeholder="placeholder"
@@ -76,6 +76,7 @@ const test= ref('')
     variant="number"
     text="Disabled"
     placeholder="placeholder"
+    disabled
   />
   <Inputvue
     v-model="test"
