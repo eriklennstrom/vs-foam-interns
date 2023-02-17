b9a09fde3c638e57395fd67b547ede5735cb524e
timand1 <timandersson22@live.se>
2023-02-17 13:24:44 +0100

    Initial files select v1

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,36 @@
 <script setup lang="ts">
-// import ButtonVue from './components/Button/Button.vue';
-// import Icons from '@/components/icons/icons.vue'
-// import Badge from '@/components/badge/badge.vue'
-// import Buttonvue from '@/components/button/button.vue'
-import Inputvue from '@/components/input/input.vue'
-import { ref, type Ref } from 'vue';
-
+import FoamSelect from '@/components/select/select.vue'
+import FoamButton from '@/components/button/button.vue'
 
 const darkMode: () => void = () => {
   const body = document.querySelector('body')
   body?.classList.toggle('dark')
-  body?.classList.contains('dark') ? body.style.backgroundColor = '#002737' : body ? body.style.backgroundColor = '#F6F8FA' : null
+  body?.classList.contains('dark') ? body.style.backgroundColor = '#1F252F' : body? body.style.backgroundColor = '#F6F8FA' : null
 }
-const test= ref('')
+
+const changeFunc: (item: string) => void = (item) => { 
+  console.log(item);  
+}
 
 </script>
 
 <template>
-  <section>
-    <h1>{{ test }}</h1>
-  
-    <Inputvue
-      v-model="test"
-      size="sm"
-      validation-text="Validation-text"
-      variant="number"
-      label="Valid"
-      placeholder="placeholder"
-      helpertext="Helper text"
+  <div class="wrapper">
+    <FoamSelect 
+      text="To Do" 
+      :options="['kaka', 'dator', 'test', 'kaffe']" 
+      @change="changeFunc"
     />
-
-    <Inputvue
-
-      v-model="test"
-      :is-valid="true"
-      validation-text="Validation-text"
-      variant="email"
-      label="Valid"
-      placeholder="placeholder"
-      helpertext="Helper text"
+    
+    <FoamSelect
+      text="To Do"
+      :options="['kaka', 'dator', 'test', 'kaffe']"
+      position="top"
+      @change="changeFunc"
     />
-    <Inputvue
-      v-model="test"
-      size="sm"
-      :is-valid="false"
-      validation-text="Validation-text"
-      variant="text"
-      label="Not valid"
-      placeholder="placeholder"
-      helpertext="Helper text"
-    />
-
-
-    <Inputvue
-      v-model="test"
-      size="lg"
-      variant="text"
-      label="Neutral"
-      placeholder="placeholder"
-      helpertext="Helper text"
-    >
-      <template #sentContent>
-        <div>
-          <p>Sent content</p><p>Sent content</p><p>Sent content</p><p>Sent content</p><p>Sent content</p><p>Sent content</p><p>Sent content</p><p>Sent content</p>
-        </div>
-      </template>
-    </Inputvue>
-
-    <Inputvue
-      v-model="test"
-      variant="number"
-      label="Disabled"
-      placeholder="placeholder"
-      disabled
-    />
-    <Inputvue
-      v-model="test"
-      :is-valid="false"
-      validation-text="validation-text"
-      variant="password"
-      label="Password"
-      placeholder="placeholder"
-    />
-  
-
-    <Inputvue
-      v-model="test"
-      :is-valid="true"
-      validation-text="validation-text"
-      variant="password"
-      label="Password"
-      placeholder="placeholder"
-    >
-      <template #helperMessageText>
-        Helper text
-      </template>
-    </Inputvue>
-
-
-    <button @click="darkMode">
-      DARK MODE!
-    </button>
-  </section>
+    
+    <FoamButton text="Dark Mode" variant="primary" @click="darkMode" />
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -109,15 +38,18 @@ const test= ref('')
 body {
   background-color: #F6F8FA;
 }
-
-section {
-  margin-bottom: 4em;
-  margin-top: 4em;
+.wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  gap: 2em;
+  margin: 4em;
 }
 
 body{
   background-color: #668592;
-;
+
 }
 
 </style>
---
+++ src/components/select/select.vue
@@ -0,0 +1,162 @@
+<script setup lang="ts">
+import Icon from '@/components/icons/icons.vue';
+import { computed, ref, useSlots, watch, watchEffect, type Ref } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+import { createPopper } from '@popperjs/core';
+import useDetectOutsideClick from '@/composables/clickOutsideComponent'
+
+type SelectProps = {
+    text?: string
+    placeholder?: string
+    position?: string
+    options?: string[] | null
+};
+const props = withDefaults(defineProps<SelectProps>(), {
+    text: 'Label',
+    position: 'inside',
+    placeholder: '',
+    options : null
+});
+
+const emit = defineEmits(['change']);
+
+const showOptions: Ref = ref<boolean>(false);
+const selectId = ref<string>('select-' + uuidv4())
+const buttonId = ref<string>('btn-' + uuidv4())
+const selectRef: Ref = ref()
+const userInput = ref<string>('')
+const labelPosition = ref<string>(props.position)
+const selectedOption = ref<string>('')
+
+
+watch(() => userInput.value, (newVal) => { 
+  const optionElems = document.querySelectorAll(`#${selectId.value} .option`)
+
+
+  selectRef.value.setAttribute('data-show', '')
+  const inputLowerCase = newVal.toLowerCase()
+  Array.from(optionElems).forEach(function (element) {  
+    element.textContent?.toLowerCase().includes(inputLowerCase) ? element.classList.remove('removed') : element.classList.add('removed')
+  });
+  
+
+});
+const popperInstance = computed(() => {
+  const buttonElem = document.querySelector(`#${buttonId.value}`) as HTMLElement
+  const dropdownElem = document.querySelector(`.${selectId.value}`) as HTMLElement
+  return createPopper(buttonElem, dropdownElem, {
+    placement: 'auto-start',
+    modifiers: [
+      {
+        name: 'flip',
+        options: {
+          allowedAutoPlacements: [`bottom-end`, `top-end`]
+        },
+      },
+      {
+        name: 'offset',
+        options: {
+          offset: [0, props?.position == 'top' ? 12 : 6],
+        }
+      }
+    ],
+    strategy: 'absolute'
+  });
+})
+const handleShowOptions: () => void = () => {
+  showOptions.value = !showOptions.value
+  
+  if(showOptions.value) {
+    const inputElem = document.querySelector(`#${selectId.value} #dropdown__input--input`) as HTMLElement  
+    if(inputElem) { // Only for Vitest
+      inputElem.focus()
+    }  
+    popperInstance.value.update()
+    selectRef.value.setAttribute('data-show', '')
+  } else {   
+    selectRef.value.removeAttribute('data-show')
+  }
+}
+const handleFocusOut: () => void = () => {
+  userInput.value = selectedOption.value
+}
+
+const handleClearInput: () => void = () => {
+  userInput.value = ''
+}
+
+const handleSelectOption: (option: string) => void = (option) => {
+    selectedOption.value == option ? selectedOption.value = '' : selectedOption.value = option
+    userInput.value = option
+    emit('change', selectedOption.value)
+}
+
+const componentRef = ref()
+
+// ---- REBASE MAIN - ADD TAB TRAP REMOVE COMPOSABLE FOLDER ----
+
+// Close dropdown on click outside the component
+useDetectOutsideClick(componentRef, () => { 
+    showOptions.value = false
+    selectRef.value.removeAttribute('data-show')
+})
+
+</script>
+
+<template>
+  <section
+    :id="selectId"
+    ref="componentRef"
+    class="select"
+    @click="handleShowOptions"
+  >
+    <div :id="buttonId" class="select--container">
+      <div class="input-container">
+        <input
+          id="select--input"
+          v-model="userInput"
+          type="text"
+          class="subtitle2"
+          name="select-input"
+          :placeholder="labelPosition == 'inside' ? ' ' : props.placeholder"
+          @focus="handleClearInput"
+          @focusout="handleFocusOut"
+        >
+        <label
+          class="select--label subtitle2"
+          :class="[`label-${labelPosition}`]"
+          for="select-input"
+          :data-active="[showOptions && labelPosition == 'inside' ? true : false]"
+        >
+          {{ props.text }}
+        </label>
+        <button class="select--button">
+          <Icon
+            :class="[showOptions ? 'options-active' : null]"
+            icon="caret-down"
+            :size="16"
+          />
+        </button>
+        <div
+          id="option__container"
+          ref="selectRef"
+          :class="selectId"
+        >
+          <div
+            v-for="option in props.options"
+            :key="option"
+            :class="selectedOption == option ? 'option--selected option' : 'option'"
+            @click="handleSelectOption(option)"
+            @keyup.enter="handleSelectOption(option)"
+          >
+            {{ option }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+    @import "@/components/select/select.scss";
+</style>
