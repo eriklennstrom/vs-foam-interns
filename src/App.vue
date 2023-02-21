decafebab1cbf8c7207e16517d24963ea8ea7d17
Christian Johnsson <chjohnsson@hotmail.com>
2023-02-21 15:55:32 +0100

    Christian Number input (#16)
    The new number input feature allows for smaller number inputs, with selectable direction, a changeable label, dynamic sizing linked to maxLength, error-handling with icon and text colors, helper-text to guide the user, and disable options, as well as being functional in Storybook and with tests

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,56 @@
 <script setup lang="ts">
+import { ref, type Ref } from 'vue';
+import NumberInput from './components/number-input/number-input.vue';
+
+const test: Ref<number> = ref(0);
 
 const darkMode: () => void = () => {
-  const body = document.querySelector('body')
-  body?.classList.toggle('dark')
-  body?.classList.contains('dark') ? body.style.backgroundColor = '#1F252F' : body? body.style.backgroundColor = '#F6F8FA' : null
-}
+  const body = document.querySelector('body');
+  body?.classList.toggle('dark');
+  body?.classList.contains('dark')
+    ? (body.style.backgroundColor = '#1F252F')
+    : body
+    ? (body.style.backgroundColor = '#F6F8FA')
+    : null;
+};
 </script>
 
-
-<template>  
+<template>
+  <p>{{ test }}</p>
   <button @click="darkMode">
     DARK MODE!
   </button>
-  <div style="height: 100vh" />
+
+  <section>
+    <NumberInput
+      v-model="test"
+      direction="vertical"
+      label="Label" 
+      helpertext="Helpertext"
+      :is-valid="true"
+      :maxlength="3"
+    />
+
+    <NumberInput
+      v-model="test"
+      direction="horizontal"
+      label="Label" 
+      helpertext="Helpertext"
+      :is-valid="true"
+      :maxlength="5"
+      :maxvalue="99999"
+    />
+  </section>
 </template>
 
 <style scoped lang="scss">
 
-article {
+body {
+  background-color: #F6F8FA;
+}
+
+section {
+  
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
---
+++ src/components/number-input/number-input.stories.ts
@@ -0,0 +1,241 @@
+import FoamNumbers from '@/components/number-input/number-input.vue';
+import type { Meta, StoryFn } from '@storybook/vue3';
+import { ref, type Ref } from 'vue';
+import numberInputDocs from './number-input.md?raw';
+
+export default {
+  title: 'components/NumberInput',
+  component: FoamNumbers,
+  parameters: {
+    docs: {
+      description: {
+        component: numberInputDocs
+      }
+    }
+  },
+  argTypes: {
+    label: {
+      control: { type: 'text'},
+      table: {
+        default: 'Label'
+      }
+    },
+    isValid: {
+      control: { type: 'select' },
+      options: [true, false, null],
+      table: {
+        defaultValue: null
+      }
+    },
+    
+    direction: {
+      control: { type: 'select' },
+      options: ['horizontal','vertical'],
+      table: {
+        defaultValue: 'horizontal',
+      }
+    },
+
+    disabled: {
+      control: { type: 'boolean' },
+      table: {
+        defaultValue: false,
+      }
+    },
+    placeholder: {
+      control: { type: 'number' },
+      table: {
+        disable:true,
+        defaultValue: 0,
+      }
+    },
+    helpertext: {
+      control: { type: 'text' },
+      table: {
+        defaultValue: '',
+      }
+    },
+    maxlength: {
+      control: { type: 'range', min:1, max:14 },
+      table: {
+        defaultValue: '',
+      }
+    },
+    defaultvalue: {
+      control: { type: 'number' },
+      table: {
+        disable:true,
+        defaultValue: 0,
+      }
+    },
+    increment: {
+      control: { type: 'number' },
+      table: {
+        defaultValue: 1,
+      }
+    },
+    maxvalue: {
+      control: { type: 'number' },
+      table: {
+        defaultValue: 999999,
+      }
+    },
+    validationText: {
+      control: { type: 'text' },
+      table: {
+        defaultValue: 'validation text',
+      }
+    },
+    change: {
+      table: {
+        disable: true,
+      }
+    },
+    
+    'update:modelValue': {
+      table: {
+        disable: true,
+      }
+    },
+    modelValue:{
+      table: {
+        disable: true,
+      }
+    }
+  }
+} as Meta<typeof FoamNumbers>;
+
+ const Template: StoryFn<typeof FoamNumbers> = (args) => ({
+  components: { FoamNumbers },
+  setup() {
+    const test:Ref= ref(0)
+    return { args, test };
+
+  },
+  template: '<foam-numbers v-bind="args" v-model="test" />'
+}); 
+
+
+export const Default = Template.bind({});
+Default.args = {
+ };
+
+
+export const Variants: StoryFn<typeof FoamNumbers> = (args, { argTypes }) => ({
+  components: { FoamNumbers },
+  setup() {
+    const test:Ref= ref(0)
+    return { args, argTypes, test };
+  },
+  template: `
+    <div style="display: flex; gap:1em">
+      <foam-numbers v-for="direction in argTypes.direction.options"  v-bind="args" :direction="direction" v-model="test" />
+    </div>`
+});
+
+Variants.argTypes = {
+  label: {
+    label: 'Label'
+  },
+
+  direction: {
+    table: {
+      disable: true
+    }
+  }
+};
+
+
+
+export const Validation: StoryFn<typeof FoamNumbers> = (args, { argTypes }) => ({
+  components: { FoamNumbers },
+  setup() {
+    const test:Ref= ref(0)
+    return { args, argTypes, test };
+  },
+  template: `
+    <div style="display: flex; gap: 1em; flex-wrap: wrap">
+      <foam-numbers v-for="options in argTypes.isValid.options" v-bind="args" :isValid=options v-model="test" />
+    </div>
+    <div style="display: flex; gap: 1em; flex-wrap: wrap">
+    <foam-numbers v-for="options in argTypes.isValid.options" v-bind="args" :isValid=options  direction="vertical" v-model="test" />
+  </div>`
+});
+
+Validation.argTypes = {
+
+  label: {
+    label: 'Label'
+  },
+  validationText: {
+    validationText: 'validationText'
+  },
+  variant: {
+    table: {
+      disable: true
+    }
+  },
+};
+
+export const Helper: StoryFn<typeof FoamNumbers> = (args, { argTypes }) => ({
+  components: { FoamNumbers },
+  setup() {
+    const test:Ref= ref(0)
+    return { args, argTypes, test };
+  },
+  template: `
+      <foam-numbers v-bind="args" helpertext="Helpertext" v-model="test" />`
+});
+
+Helper.argTypes = {
+
+  label: {
+    label: 'Label'
+  },
+  validationText: {
+    validationText: 'validationText'
+  },
+  variant: {
+    table: {
+      disable: true
+    }
+  },
+};
+
+export const Disabled: StoryFn<typeof FoamNumbers> = (args, { argTypes }) => ({
+  components: { FoamNumbers },
+  setup() {
+    const test:Ref= ref(0)
+    return { args, argTypes, test };
+  },
+  template: `
+      <foam-numbers v-bind="args" disabled v-model="test" />`
+});
+
+Disabled.argTypes = {
+
+  label: {
+    label: 'Label'
+  },
+  validationText: {
+    validationText: 'validationText'
+  },
+  variant: {
+    table: {
+      disable: true
+    }
+  },
+};
+
+
+
+
+
+
+Variants.args = {
+  label: 'Label'
+ };
+
+ Validation.args = {
+  label: 'Label'
+ };
